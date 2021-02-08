c85b26b2c11475eb8111d43ffac8f051f387de09
Maju_kohler <mariajuliakohler@gmail.com>
2021-02-08 16:17:08 -0300

    first entry

---
+++ Day1/introduction.js
@@ -0,0 +1,8 @@
+console.log('Welcome to 30 days of Javascript')
+
+//comments can make code readable
+//Welcome to 30 days of Javascript
+
+/*
+comments can make code readable,
+easy to reuse and informative */
---
+++ Day1/variables.js
@@ -0,0 +1,46 @@
+let nome = 'Maria';
+let isStudent = true;
+let age
+let financialCondition = null;
+
+console.log(typeof nome)
+console.log(typeof isStudent)
+console.log(typeof age)
+console.log(typeof financialCondition)
+
+//----------------------------------------------------------------
+//undeclared variables
+let pessoa
+let cidade
+let estado
+let partnership
+
+//declares variables
+let mercado = 'Broto da Terra'
+let fruta = 'banana'
+let verdura = 'abobrinha'
+let quantidadeVerdura = 3
+
+//----------------------------------------------------------------
+//multiple variable lines
+let firstName = 'Maria Júlia'
+let lastName = 'Köhler'
+let MaritalStatus = 'single'
+let country = 'Brazil'
+let age = 19
+
+//----------------------------------------------------------------
+//one line, multiple variables
+let FirstName = 'Maria Júlia',
+    lastName = 'Köhler',
+    MaritalStatus = 'single',
+    country = 'Brazil',
+    age = 19
+
+//----------------------------------------------------------------
+
+let myAge = 19
+let yourAge = 30
+
+console.log('I am ' + myAge + ' years old')
+console.log('You are ' + yourAge + ' years old')
